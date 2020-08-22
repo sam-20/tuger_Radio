0268461b1679ce94fed38268489325e8d2ba2233
samuel <botwesamuel192@gmail.com>
2020-08-22 17:37:20 +0000

    cleaned soundplayer files

--- android/app/src/main/java/com/radio/MainApplication.java
+++ android/app/src/main/java/com/radio/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.guichaguri.trackplayer.TrackPlayer;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
